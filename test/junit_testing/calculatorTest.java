fbffd5f5c8b1cf74f96fc6994e1959ea59f43067
Lucky Pratama <luckypratama71@yahoo.com>
2017-09-29 21:58:33 +0300

    delete unused class

--- test/junit_testing/calculatorTest.java
+++ test/junit_testing/calculatorTest.java
@@ -4,7 +4,7 @@ import main.calculator;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+
 /**
  *
  * @author Lucky Pratama
